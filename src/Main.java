b3d88067f1391f157c497b13ba490dbe0a56d79b
JoshuaPraveen99 <65905555+JoshuaPraveen99@users.noreply.github.com>
2023-09-05 23:00:41 +0530

    Array declaration and intialization

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        int[]arr= new int[5];
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+        arr[3]=40;
+        arr[4]=50;
+        for(int a:arr){
+            System.out.println(a);
+        }
     }
 }
