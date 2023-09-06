bae5fd6ccc5900c4fcb404db19aa36688086c1bf
JoshuaPraveen99 <65905555+JoshuaPraveen99@users.noreply.github.com>
2023-09-06 15:55:54 +0530

    Multidimensional and Dynamic Arrays

--- src/DynamicArrays.java
+++ src/DynamicArrays.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class DynamicArrays {
+import java.util.ArrayList;
+
+public class DynamicArrays {
+    public  static void main(String[]args){
+
+        //We can use arraylist in java when we don't know the size of the array
+        ArrayList<Integer> list= new ArrayList<>(10); // we can't use primitives , we should only use wrapper classes.
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        list.add(11);
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(15);
+        list.add(16);
+
+
+        list.set(1,100);
+        list.remove(14);
+        System.out.println(list);
+
+
+
+
+
+
+
+
+
+
+    }
 }
--- src/MultidimensionalArrays.java
+++ src/MultidimensionalArrays.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class MultidimensionalArrays {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultidimensionalArrays {
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        /*
+        int[][]arr= new int[3][3]; // mentioning the size of rows is necessary but mentioning the size of column is not necessary
+        int[][]arr1={
+                {1,2,3},
+                {4,5,6},
+                {6,7,8}
+        };
+
+        System.out.println(Arrays.toString(arr1[1]));  // output: {4,5,6}
+        System.out.println(arr1.length); // give no. of rows
+
+        int[][]arr2={
+                {1,2,3},
+                {4,5},
+                {6,7,8,9}
+        };                   // the column size can vary for each array
+
+        // traversing logic for multidimensional arrays
+           for(int row=0; row<arr.length; row++){
+            for(int col=0; col<arr[row].length; col++){
+                  arr[row][col]= sc.nextInt();
+            }
+        }*/
+
+        int[][]arr2= {
+                {1,2,3},
+                {4,5},
+                {6,7,8,9}
+        };
+        for(int row=0; row<arr2.length; row++){
+            for(int col=0; col<arr2[row].length; col++) {
+                System.out.print(arr2[row][col] + " ");
+            }
+            System.out.println();
+        };
+
+        for(int[]a:arr2){
+            System.out.println(Arrays.toString(a));
+        }    // since in multidimensionsal arrays each row represent a individual array
+
+        //We can use arraylist in java which can be used when we dont know the size of the array.
+
+
+
+    }
 }
